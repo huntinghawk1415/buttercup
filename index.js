622c865ee673a12efc9925220d4cf01566aee4f2
Jacob Lockett <jacobwyattlockett@gmail.com>
2020-01-24 23:07:48 -0500

    complete

--- index.js
+++ index.js
@@ -1,22 +1,40 @@
+const storage = window.localStorage
+const body = document.querySelector('body')
 const auth = document.querySelector('#auth')
-const form = document.querySelector('#auth-form')
 const main = document.querySelector('#main')
 
-form.addEventListener('submit', e => {
-  e.preventDefault()
-
-  const password = form['password-input'].value
-
-  /**
-   * If you're not supposed to be here - congrats.
-   * You know your way around whichever browser's devtools you're using.
-   * Unfortunately, this is the world's least secure password...
-   * But good for you hackerman.
-   * Claps all around for f12.
-   */
-  
-  if (password === '1234') {
-    auth.classList.add('hidden')
-    main.classList.remove('hidden')
-  }
+lightbox.option({
+  maxWidth: 700,
+  resizeDuration: 0,
+  wrapAround: true
 })
+
+if (storage.getItem('buttercupsandfluffys')) {
+  auth.classList.add('hidden')
+  body.classList.add('frame')
+  main.classList.remove('hidden')
+} else {
+  const form = document.querySelector('#auth-form')
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+  
+    const password = form['password-input'].value
+  
+    /**
+     * If you're not supposed to be here - congrats.
+     * You know your way around whichever browser's devtools you're using.
+     * Unfortunately, this is the world's least secure client-side javascript password...
+     * I mean, hell, the damn HTML is in the body if you want to just copy it over to CodePen or something.
+     * But, no, good for you hackerman.
+     * Claps all around for f12.
+     */
+    
+    if (password === 'Floofy') {
+      storage.setItem('buttercupsandfluffys', true)
+      auth.classList.add('hidden')
+      body.classList.add('frame')
+      main.classList.remove('hidden')
+    }
+  })
+}
